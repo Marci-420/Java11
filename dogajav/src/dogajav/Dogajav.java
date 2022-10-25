069040e77f2b5fc21bf503643b2d043e0460c357
Marci-420 <meszaros.marcell.zsolt.2020i@bankitatabanya.hu>
2022-10-25 10:30:53 +0200

    doga javítás

---
+++ dogajav/src/dogajav/Dogajav.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dogajav;
+
+/**
+ *
+ * @author user3
+ */
+public class Dogajav {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String szoveg2 = "Soki lúd disznót győz.";
+        //1
+        System.out.println(szoveg2.length());
+        //2 
+        System.out.println(szoveg2.charAt(0));
+        //3
+        System.out.println(szoveg2.equals(szoveg2, masikszoveg));
+        //4
+        System.out.println(szoveg2.contain("lúd"));
+        //5
+        System.out.println(szoveg2.indexOf("lúd"));
+        //6
+        System.out.println(szoveg2.replace("lúd", "veréb"));
+        
+        
+         int xddd = szoveg2.indexOf(" ");
+         System.out.println(szoveg2.substring(xddd));
+    }
+    
+}
