a47e91934458070beb8630ac09ba5f54476c283f
Marci-420 <meszaros.marcell.zsolt.2020i@bankitatabanya.hu>
2022-11-04 12:59:41 +0100

    metódusok(részben)

---
+++ Metodusoksz/src/metodusoksz/Metodusoksz.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodusoksz;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user3
+ */
+public class Metodusoksz {
+    
+    //metódusok és osztályok helye
+    /**
+     *
+     */
+    
+    
+    public static double atlag(int szam, int szam2) {
+        double atlag = (szam + szam2) / 2.0;
+        return atlag;
+        
+        
+    }
+        
+    public static int veletlen(ont also, int felso) {
+        
+   
+  }
+}
